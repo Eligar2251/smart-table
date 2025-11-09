f1cb7e203b3846ae2fc4fd3cdc045575bac1061b
Eligar2251 <eligar225@yandex.ru>
2025-11-10 00:18:31 +0700

    test1

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,39 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
-
-// @todo: #4.3 — настроить компаратор
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+
+    Object.keys(indexes)
+        .forEach((elementName) => {
+            if (elements[elementName]) {
+                elements[elementName].append(
+                    ...Object.values(indexes[elementName])
+                        .map(name => {
+                            const option = document.createElement('option');
+                            option.value = name;
+                            option.textContent = name;
+                            return option;
+                        })
+                );
+            }
+        });
 
     return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const parentElement = action.parentElement;
+            if (parentElement) {
+                const inputToClear = parentElement.querySelector('input');
+                if (inputToClear) {
+                    inputToClear.value = '';
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+                    const fieldName = action.dataset.field;
+                    if (fieldName && state.hasOwnProperty(fieldName)) {
+                        delete state[fieldName];
+                    }
+                }
+            }
+        }
+
+        const compare = createComparison(defaultRules);
+        return data.filter(row => compare(row, state));
     }
 }
